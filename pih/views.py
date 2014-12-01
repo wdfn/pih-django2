a6226f915b298cc178ee747d735d7272633a6216
Kishan Panchal <kpanchal7@berkeley.edu>
2014-12-01 09:26:28 -0800

    created all pages, added routing, but not all routing works when you click on different sections of the navbar

--- pih/views.py
+++ pih/views.py
@@ -7,7 +7,7 @@ def home(request):
 
 
 def aboutus(request):
-	context = {}
+	context = {}  
 	template = "about-us.html"
 	return render(request, template, context)	
 
@@ -20,4 +20,40 @@ def blog(request):
 	context = {}
 	template = "blog.html"
 	return render(request, template, context)	
+
+
+def ourwork(request):
+	context = {}
+	template = "ourwork.html"
+	return render(request, template, context)		
+
+def mission(request):
+	context = {}
+	template = "mission.html"
+	return render(request, template, context)	
+
+def history(request):
+	context = {}
+	template = "history.html"
+	return render(request, template, context)	
+
+def founders(request):
+	context = {}
+	template = "founders.html"
+	return render(request, template, context)	
+
+def governance(request):
+	context = {}
+	template = "governance.html"
+	return render(request, template, context)	
+
+def events(request):
+	context = {}
+	template = "events.html"
+	return render(request, template, context)	
+
+def donate(request):
+	context = {}
+	template = "donate.html"
+	return render(request, template, context)			
 
